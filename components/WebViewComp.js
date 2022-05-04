f121b0d4928d112d142db5538095382bf37269e8
Shivam <19148@iiitu.ac.in>
2022-05-04 10:23:40 +0530

    changed splash scree

--- components/WebViewComp.js
+++ components/WebViewComp.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {View,BackHandler,Platform,StyleSheet} from 'react-native';
+import {View,BackHandler,Platform,StyleSheet, Image} from 'react-native';
 import {WebView} from 'react-native-webview';
 import * as Progress from 'react-native-progress';
 
@@ -32,7 +32,12 @@ hideSpinner() {
     }
     return false;
   }
-
+  componentDidMount(){  
+    var that = this;  
+    setTimeout(function(){  
+      that.hideSpinner();  
+    }, 5000);  
+   }  
   componentWillMount() {
     if (Platform.OS === 'android') {
       BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
@@ -45,6 +50,13 @@ hideSpinner() {
     }
   }
   render() {
+    let Splash_Screen = (  
+      <View style={styles.SplashScreen_RootView}>  
+          <View style={styles.SplashScreen_ChildView}>  
+                <Image source={require('../assets/logo2.png')}  
+             style={{width:'100%', height: '100%', resizeMode: 'contain',backgroundColor: '#fff'}} />  
+         </View>  
+      </View> )  
     return (
       <View style={this.state.visible === true ? styles.stylOld : styles.styleNew}>
         {this.state.visible ? (
@@ -66,11 +78,12 @@ hideSpinner() {
           javaScriptEnabled={true}
           domStorageEnabled={true}
           startInLoadingState={true}
+          renderLoading={()=>(this.state.visible === true) ? Splash_Screen : null  }
           style={{marginTop: 35}}
           onLoadStart={() => (this.showSpinner())}
           onLoadProgress={() => (this.setProgress())}
           onLoad={() => (this.hideSpinner())}
-        />
+        /> 
     </View>
     )
   }
@@ -89,12 +102,22 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 40,
   },
-  ActivityIndicatorStyle: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-  },
+  SplashScreen_RootView:  
+    {  
+        justifyContent: 'center',  
+        flex:1,    
+        position: 'absolute',  
+        width: '100%',  
+        height: '100%',  
+      },  
+   
+    SplashScreen_ChildView:  
+    {  
+        justifyContent: 'center',  
+        alignItems: 'center',  
+        backgroundColor: '#fff',  
+        flex:1,  
+    },  
   progress:{
     top:35
   }
